221de1cb00005e9cee174cdc4ad46c8189ad0836
nagarajaneesh <nagarajaneesh02@gmail.com>
2026-01-24 13:57:38 +0530

    dev-express

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (_req, res) => {
+  res.send("in dev branch");
+});
+
+app.listen(3000, (err) => {
+  if (err) console.log("error starting sevrer");
+  else console.log("started server on 3000");
+});
